337e599bae0145bd2fc79f08e2072013eae8e6e2
walaa-khafagy <87380732+walaa-khafagy@users.noreply.github.com>
2026-04-19 14:21:58 +0200

    chore: setup project structure, routing, and add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StudentDetailsComponent } from './components/student-details/student-details.component';
+import { StudentTableComponent } from './components/student-table/student-table.component';
+import { StudentsComponent } from './components/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    HomeComponent,
+    NavbarComponent,
+    StudentDetailsComponent,
+    StudentTableComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/student-table/student-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-table',
+  templateUrl: './student-table.component.html',
+  styleUrls: ['./student-table.component.css']
+})
+export class StudentTableComponent {
+
+}
