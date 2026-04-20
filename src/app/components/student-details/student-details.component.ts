fcd837767f31feadfccfc93ebbf3438341ee2bad
walaa-khafagy <87380732+walaa-khafagy@users.noreply.github.com>
2026-04-20 13:01:51 +0200

    feat: implement student details component

--- src/app/components/student-details/student-details.component.ts
+++ src/app/components/student-details/student-details.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { combineLatest, map, Observable } from 'rxjs';
+import { Student } from '../../interfaces/student';
+import { StudentsService } from '../../services/students.service';
 
 @Component({
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
-  styleUrls: ['./student-details.component.css']
+  styleUrls: ['./student-details.component.css'],
 })
 export class StudentDetailsComponent {
+  readonly viewModel$: Observable<{ student: Student | undefined }>;
 
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly studentsService: StudentsService,
+  ) {
+    this.viewModel$ = combineLatest([
+
+      this.route.paramMap,
+      this.studentsService.students$,
+
+    ]).pipe(
+
+      map(([params, students]: [ParamMap, Student[]]) => {
+        const id = Number(params.get('id'));
+        if (Number.isNaN(id)) {
+          return { student: undefined };
+        }
+        return { student: students.find((s: Student) => s.id === id) };
+      }),
+
+    );
+  }
 }
